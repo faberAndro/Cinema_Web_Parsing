c41be9b7ca22ec2bf8e73e81bf24a9b6681419c8
FabIT <40968674+faberAndro@users.noreply.github.com>
2020-06-24 02:08:42 +0100

    Add files via upload

--- ExtractNumOfMovies_GIT.py
+++ ExtractNumOfMovies_GIT.py
@@ -55,8 +55,8 @@ def find_descriptive_parameters(first_page_text):
     return text_to_print, y
 
 
-def write_page_result_in_csv_file(file_di_output, *args):
-    film_file = csv.writer(file_di_output, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+def write_page_result_in_csv_file(file_output, *args):
+    film_file = csv.writer(file_output, delimiter=',', quotechar='\"', quoting=csv.QUOTE_ALL)
     pagina = "page " + str(page_number)
     riga = [args[0]] + [args[1]] + [args[2]] + args[3]
     riga.insert(0, pagina)
@@ -68,17 +68,17 @@ if __name__ == '__main__':
     page_number = 0
     n_results_in_current_year, n_films, all_TITLES_in_page, last_page = 0, 0, [], False     # last_page e' la last_page delle pagine relative all'anno corrente
 
-    current_year = input('Anno? ')
+    current_year = input('Year? ')
     current_year = int(current_year)
     indirizzo = page_address_parameters()
 
-    with open('films.csv', 'w', newline='') as file_di_output:
+    with open('films.csv', 'w', newline='') as file_output:
         while not last_page:
             page_number += 1
             text_of_current_page = extract_page(page_number)
             if page_number == 1:
                 header, n_results_in_current_year = find_descriptive_parameters(text_of_current_page)
-                print(header, n_results_in_current_year)
+                print('Results: ', n_results_in_current_year)
             tree = html.fromstring(text_of_current_page)
             all_TITLES_in_page = tree.xpath('//h2/a//@title')
             all_DIRECTORS_in_page = tree.xpath('//b//a[@href]/text()')
@@ -92,7 +92,7 @@ if __name__ == '__main__':
             """
             n_films = len(all_TITLES_in_page)
             n_results_in_current_year -= n_films
-            print("\nPagina: %d -- Numero films: %d -- Film rimanenti nell'anno %d: %d" % (page_number, n_films, current_year, n_results_in_current_year))
+            print("\nPage: %d -- Number of movies: %d -- Movies left within year %d: %d" % (page_number, n_films, current_year, n_results_in_current_year))
             time.sleep(1)
             i, j = 0, 0
             for i in range(len(actors_and_genres)):
@@ -102,11 +102,11 @@ if __name__ == '__main__':
                 else:
                     genere = attori[len(attori)-1]
                     attori.remove(genere)
-                    print("\n*** FILM: %s\n** REGISTA: %s\n**** ATTORI [%s]:  %s\n* GENERE: %s" %
+                    print("\n*** MOVIE: %s\n** DIRECTOR: %s\n**** ACTORS [%s]:  %s\n* GENRE: %s" %
                           (all_TITLES_in_page[j], all_DIRECTORS_in_page[j], len(attori), ', '.join(attori), genere))
-                    write_page_result_in_csv_file(file_di_output, all_TITLES_in_page[j], all_DIRECTORS_in_page[j], genere, attori)
+                    write_page_result_in_csv_file(file_output, all_TITLES_in_page[j], all_DIRECTORS_in_page[j], genere, attori)
                     attori.clear()
                     j +=1
             if n_results_in_current_year == 0:
                 last_page = True
-    file_di_output.close()
+    file_output.close()
